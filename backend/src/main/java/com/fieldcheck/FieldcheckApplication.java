471124bb082975c5eff100acfe97bf35111dd823
EnilioFIME <emilio.romeroc@uanl.edu.mx>
2026-04-15 09:33:04 -0600

    feat: prueba a servidor local usando Springboot y Maven, faltan credenciales

---
+++ backend/src/main/java/com/fieldcheck/FieldcheckApplication.java
@@ -0,0 +1,17 @@
+package com.fieldcheck;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+
+public class FieldcheckApplication {
+    
+// Prueba chica, si levanta el servidor pero se necesitan tener las credenciales correspondientes
+
+    public static void main(String[] args) {
+        
+
+        SpringApplication.run(FieldcheckApplication.class, args);
+    }
+}
